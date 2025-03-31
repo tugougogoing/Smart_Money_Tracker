da27f011a1b7682d2d5b55295af6d279a5074c84
Jiang-Niao <1732500265@qq.com>
2025-03-31 21:34:12 +0800

    :update: 修改文案

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,14 +12,11 @@ export interface TransactionLogData {
 
 export function formatTransactionOutput(data: TransactionLogData): string {
     return [
-        '————————————————————————————',
-        `交易槽位：${data.slot}`,
-        `交易钱包：${data.wallet}`,
+        `交易钱包：\`${data.wallet}\``,
         `交易合约：${data.contract}`,
         `交易类型：${data.type}`,
-        `代币地址：${data.tokenMint}`,
+        `代币地址：\`${data.tokenMint}\``,
         `SOL数量：${formatTokenAmount(Math.abs(data.solAmount))}`,
         `代币数量：${formatTokenAmount(Math.abs(data.tokenAmount))}`,
-        '————————————————————————————\n'
     ].join('\n');
 } 
